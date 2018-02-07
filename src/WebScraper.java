37aaf19134af659a7267b44fd94c404f4557b270
yanghuipang <yanghui2@illinois.edu>
2018-02-07 12:44:51 -0600

    Finished WebScraper.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,45 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    /**
+     *
+     * @param url url
+     * @return the count
+     */
+    public static int count(final String word, final String url) {
+        int count = 0;
+        for (int i = 0; i < urlToString(url).length(); i++) {
+            if (urlToString(url).equals(word)) {
+                count++;
+            }
+        }
+        return 0;
+    }
+
+
+
     public static void main(String[] unused) {
-        System.out.println("Ran");
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
     }
 }
